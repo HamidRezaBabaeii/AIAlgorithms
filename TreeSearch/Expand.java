66972c9e89af8518b85802da6ee1abed1e660712
Hamidreza Babaei <babaiee.hamidreza@gmail.com>
2023-11-05 21:39:52 +0330

    Add expand class

--- TreeSearch/Expand.java
+++ TreeSearch/Expand.java
@@ -1,8 +1,28 @@
 package AIAlgorithms.TreeSearch;
 
+import java.util.ArrayList;
+
 public class Expand {
 
-    public Expand(){
+    // variables 
+    private ArrayList<Node> expand_Nodes;
+    private int counterDepth = 1 ;
+    private int counterNodeNumber = 1;
+
+    //constructor
+    public Expand(Node node , int[][] proximityArray){
+        this.expand_Nodes.add(node);
+        for(int i=0 ; i< node.getNodeAction().length; i++){
+            if(node.getNodeAction()[i]!=0){
+                int node_actions[] = new int[proximityArray.length];
+                for(int j=0 ; j<proximityArray.length ; j++){
+                    node_actions[j] = proximityArray[counterNodeNumber][j];
+                }
+                Fringe fringe = new Fringe(counterNodeNumber, null, node.getNodeNum(), node_actions, node.getNodeAction()[i], counterDepth);
+                counterNodeNumber++;
+                counterDepth++;
+            }
+        }
     }
     
 }
